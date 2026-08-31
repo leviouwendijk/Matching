5ac7de66e96468fb00a5fb5748fd85101a009968
leviouwendijk <120993067+leviouwendijk@users.noreply.github.com>
2026-08-31 20:52:10 +0200

    removing testflows in publish

--- Package.swift
+++ Package.swift
@@ -5,23 +5,23 @@ import PackageDescription
 
 let package = Package(
     name: "Matching",
-    platforms: [
-        .macOS(.v13)
-    ],
+    // platforms: [
+    //     .macOS(.v13)
+    // ],
     products: [
         .library(
             name: "Matching",
             targets: ["Matching"]
         ),
-        .executable(
-            name: "matchtest",
-            targets: ["MatchingTestFlows"]
-        ),
+        // .executable(
+        //     name: "matchtest",
+        //     targets: ["MatchingTestFlows"]
+        // ),
     ],
     dependencies: [
         .package(url: "https://github.com/leviouwendijk/Tokens.git", branch: "master"),
         .package(url: "https://github.com/leviouwendijk/Position.git", branch: "master"),
-        .package(url: "https://github.com/leviouwendijk/TestFlows.git", branch: "master"),
+        // .package(url: "https://github.com/leviouwendijk/TestFlows.git", branch: "master"),
     ],
     targets: [
         .target(
@@ -31,13 +31,13 @@ let package = Package(
                 .product(name: "Position", package: "Position"),
             ],
         ),
-        .executableTarget(
-            name: "MatchingTestFlows",
-            dependencies: [
-                "Matching",
-                .product(name: "Tokens", package: "Tokens"),
-                .product(name: "TestFlows", package: "TestFlows"),
-            ]
-        ),
+        // .executableTarget(
+        //     name: "MatchingTestFlows",
+        //     dependencies: [
+        //         "Matching",
+        //         .product(name: "Tokens", package: "Tokens"),
+        //         .product(name: "TestFlows", package: "TestFlows"),
+        //     ]
+        // ),
     ]
 )
